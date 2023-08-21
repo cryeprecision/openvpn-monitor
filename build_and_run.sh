6345384ae53e5236bd0bb83f0dfb0abb66eee221
cryeprecision <muench.rico@gmail.com>
2023-08-21 13:56:03 +0200

    improve comment in build script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,4 @@
-# this file is run on the build server in the project directory!
+# this file is called from `sync.sh` and is run on the build server in the project directory!
 echo '--> building project' && cargo build --release --target x86_64-unknown-freebsd && \
     echo '--> killing old processes' && ssh root@10.0.0.1 'bash -c "killall openvpn-monitor; while pgrep openvpn-monitor > /dev/null; do sleep 1; done;"' && \
     echo '--> copying executable' && scp target/x86_64-unknown-freebsd/release/openvpn-monitor root@10.0.0.1:/usr/local/bin && \
