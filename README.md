ea0eacc797c23300fa8ae0fc9cf5781e11e6ead4
cryeprecision <muench.rico@gmail.com>
2023-08-21 08:37:45 +0200

    abstract from socket interface

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # openvpn-monitor
+
+`rsync --archive --verbose --recursive -e "ssh -l root" --exclude './target' . rico@10.0.0.114:~/rust/openvpn-monitor/`
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
+use std::ops::ControlFlow;
+
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use anyhow::{Context, Result};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
 
 const BIND_IP: &str = "0.0.0.0:7505";
-const SOCKETS: &[(&str, &str)] = &[
-    ("server1", "/var/etc/openvpn/server1/sock"),
-    ("server2", "/var/etc/openvpn/server2/sock"),
-];
 
-type Socket = tokio::net::UnixStream;
-// type Socket = std::sync::Arc<std::sync::RwLock<tokio::net::UnixStream>>;
-
-pub struct State {
-    pub socket: Socket,
-}
+type BufSockRead = tokio::io::BufReader<tokio::net::unix::OwnedReadHalf>;
+type BufSockWrite = tokio::io::BufWriter<tokio::net::unix::OwnedWriteHalf>;
 
 pub fn init_logger() -> Result<()> {
     use log::LevelFilter;
@@ -41,7 +35,7 @@ struct OpenVpn<'a> {
 }
 
 impl<'a> OpenVpn<'a> {
-    pub fn from_output(server: &'a str, welcome: &'a str, output: &'a str) -> OpenVpn<'a> {
+    pub fn _from_output(server: &'a str, welcome: &'a str, output: &'a str) -> OpenVpn<'a> {
         OpenVpn {
             server,
             welcome,
@@ -50,44 +44,99 @@ impl<'a> OpenVpn<'a> {
     }
 }
 
+struct OpenVpnMgmnt {
+    sock_read: BufSockRead,
+    sock_write: BufSockWrite,
+}
+
+impl OpenVpnMgmnt {
+    pub async fn open(server_name: &str) -> anyhow::Result<OpenVpnMgmnt> {
+        if !lazy_regex::regex_is_match!("^[a-z0-9]+$", server_name) {
+            anyhow::bail!("openvpn server name can only contain lowercase letters and numbers");
+        }
+
+        let sock_path = format!("/var/etc/openvpn/{}/sock", server_name);
+        let (sock_read, sock_write) = tokio::net::UnixStream::connect(&sock_path)
+            .await
+            .context("couldn't connect to openvpn socket")?
+            .into_split();
+        let mut sock_read = tokio::io::BufReader::with_capacity(2048, sock_read);
+        let sock_write = tokio::io::BufWriter::with_capacity(2048, sock_write);
+
+        let mut welcome = String::with_capacity(256);
+        let _ = sock_read
+            .read_line(&mut welcome)
+            .await
+            .context("couldn't read welcome message from socket")?;
+        log::info!("read welcome message: `{}`", welcome);
+
+        Ok(OpenVpnMgmnt {
+            sock_read,
+            sock_write,
+        })
+    }
+    pub async fn execute<F>(&mut self, cmd: &str, mut f: F) -> anyhow::Result<()>
+    where
+        F: FnMut(&str) -> std::ops::ControlFlow<()>,
+    {
+        self.sock_write
+            .write_all(cmd.as_bytes())
+            .await
+            .context("couldn't write command to socket")?;
+        self.sock_write
+            .write_u8(b'\n')
+            .await
+            .context("couldn't write newline after command to socket")?;
+        self.sock_write
+            .flush()
+            .await
+            .context("couldn't flush command to socket")?;
+
+        let mut line_buf = String::with_capacity(1024);
+        loop {
+            let _ = self
+                .sock_read
+                .read_line(&mut line_buf)
+                .await
+                .context("couldn't read next line from socket")?;
+            if f(&line_buf).is_break() {
+                break;
+            }
+            line_buf.clear();
+        }
+
+        Ok(())
+    }
+}
+
 #[actix_web::get("/openvpn/{server}")]
-async fn get_server(path: web::Path<String>) -> actix_web::Result<HttpResponse> {
-    let path = path.as_str();
-    let socket_path = match SOCKETS.iter().find(|(name, _)| *name == path) {
-        Some((_, path)) => *path,
-        None => return Ok(HttpResponse::NotFound().finish()),
+async fn get_server(path: web::Path<String>) -> HttpResponse {
+    log::info!("connecting to socket");
+    let mut openvpn = match OpenVpnMgmnt::open(&path).await {
+        Err(err) => {
+            log::error!("{}", err);
+            return HttpResponse::InternalServerError().finish();
+        }
+        Ok(v) => v,
     };
 
-    log::info!("connecting to socket");
-    let mut socket = tokio::net::UnixStream::connect(socket_path).await.unwrap();
-    let (mut sr, mut sw) = socket.split();
+    log::info!("executing command on socket");
+    if let Err(err) = openvpn
+        .execute("status 2", |line| {
+            log::info!("line: `{}`", line);
+            if line.contains("END") || line.contains("ERROR") {
+                ControlFlow::Break(())
+            } else {
+                ControlFlow::Continue(())
+            }
+        })
+        .await
+    {
+        log::error!("{}", err);
+        return HttpResponse::InternalServerError().finish();
+    }
 
-    let mut buf = vec![0u8; 4096];
-    let mut out = String::with_capacity(2048);
-    let mut out_pre = String::with_capacity(2048);
-
-    log::info!("reading welcome message");
-    let count = sr.read(&mut buf).await.unwrap();
-    out_pre.push_str(
-        std::str::from_utf8(&buf[..count])
-            .unwrap_or("invalid-utf8")
-            .trim(),
-    );
-
-    log::info!("writing to socket");
-    sw.write_all(b"status 2\n").await.unwrap();
-
-    log::info!("reading command output");
-    let count = sr.read(&mut buf).await.unwrap();
-    out.push_str(
-        std::str::from_utf8(&buf[..count])
-            .unwrap_or("invalid-utf8")
-            .trim(),
-    );
-
-    log::info!("returning result");
-    let resp = OpenVpn::from_output(path, &out_pre, &out);
-    Ok(HttpResponse::Ok().json(&resp))
+    HttpResponse::Ok().finish()
 }
 
 #[tokio::main(flavor = "current_thread")]
